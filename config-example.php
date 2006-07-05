89ef61246b7da0efd73ba7f9bdd8d0d23e83e4bd
Valery Votintsev <vvotintsev@gmail.com>
2006-07-05 11:24:31 +0300

    Added LANG and CHARSET variables in config

--- config-example.php
+++ config-example.php
@@ -1,9 +1,21 @@
 <?php
+
 // example configuration file for BugsBuddy
 
 define('DATABASE_TYPE',          'MySQL');
+
 define('DATABASE_SERVER',        'localhost');
-define('DATABASE_USER_NAME',     'root');
-define('DATABASE_USER_PASSWORD', 'password');
+
+define('DATABASE_USER_NAME',     'root');
+
+define('DATABASE_USER_PASSWORD', 'password');
+
 define('DATABASE_DATABASENAME' , 'bugsbuddy');
-?>
+
+define('LANG',          'nl');
+
+define('CHARSET',          'UTF-8');
+
+define('MAIL_CHARSET',          'UTF-8');
+
+define('DATABASE_CHARSET',          'utf8');
