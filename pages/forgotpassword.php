a8a4d37e123853e110e222c483030c68bebc868a
vot <vvotintsev@gmail.com>
2006-08-24 19:25:12 +0300

    added lang() to pages/forgotpassword.php

--- pages/forgotpassword.php
+++ pages/forgotpassword.php
@@ -17,21 +17,42 @@ function getforgotpassword() {
 }
 
 function getForgotPasswordForm() {
+
+  $returnValue = '<h1>'.lang('password_forgot').'</h1>';
+
+  $returnValue .= "<div class='info'>".lang('password_forgot_info').'</div>';
+
   if (isset($_GET) && isset($_GET['js']) && $_GET['js'] == "no") {
-    $returnValue = '';
-    $returnValue .= '<form action="index.php" method="get">';
-    $returnValue .= '<div><input type="hidden" name="js" value="no" /></div>';
-    $returnValue .= '<div><input type="hidden" name="page" value="forgotpassword" /></div>';
-    $returnValue .= '<div class="forgotpasswordlabel"><label style="width: 100;" for="mail">Mail:</label></div><div class="forgotpasswordinput"><input class="forgotpasswordinputcontent" type="text" id="mail" name="mail" value="" /></div>';
-    $returnValue .= '<div class="registerlabel"><label for="submit">Reset password:</label></div><div class="registerinput"><input class="registerinputcontent" id="submit" type="submit" value="send mail!" /></div>';
-    $returnValue .= '</form>';
+    $returnValue .= '
+<form action="index.php" method="get">';
+  <div>
+    <input type="hidden" name="js" value="no" />
+  </div>
+  <div>
+    <input type="hidden" name="page" value="forgotpassword" />
+  </div>
+  <div class="forgotpasswordlabel">
+    <label style="width: 100;" for="mail">'.lang('email').':</label>
+  </div>
+  <div class="forgotpasswordinput">
+    <input class="forgotpasswordinputcontent" type="text" id="mail" name="mail" value="" />
+  </div>
+  <div class="registerlabel">
+    <label for="submit">'.lang('password_reset').':</label>
+  </div>
+  <div class="registerinput">
+    <input class="registerinputcontent" id="submit" type="submit" value="'.lang('send_mail').'" />
+  </div>
+</form>
+';
     return $returnValue;
+
   } else {
     $returnValue = '';
     $returnValue .=  '<form method="get" target="submitFrame" onsubmit="getNewContent(\'forgotpassword\&mail=\'+document.getElementById(\'mail\').value); return false;">';
     $returnValue .=   '<div><input type="hidden" name="page" value="forgotpassword" /></div>';
-    $returnValue .=    '<div class="forgotpasswordlabel"><label for="mail">Mail:</label></div><div class="forgotpasswordinput"><input class="forgotpasswordcontent" type="text" name="mail" id="mail" value="" /></div>';
-    $returnValue .=    '<div class="registerlabel"><label for="submit">Reset password:</label></div><div class="registerinput"><input class="registerinputcontent" id="submit" type="submit" value="send mail!" /></div>';
+    $returnValue .=    '<div class="forgotpasswordlabel"><label for="mail">'.lang('email').':</label></div><div class="forgotpasswordinput"><input class="forgotpasswordcontent" type="text" name="mail" id="mail" value="" /></div>';
+    $returnValue .=    '<div class="registerlabel"><label for="submit">'.lang('password_reset').':</label></div><div class="registerinput"><input class="registerinputcontent" id="submit" type="submit" value="'.lang('send_mail').'" /></div>';
     $returnValue .=  '</form>';
     $returnValue .=  '<br />';
     $returnValue .=  '<div id="forgotPassword">';
@@ -45,23 +66,25 @@ function handleForgotPassword($mail) {
   $error = false;
   $user = Database::getUserByEmail($mail);
   if (!$user || count($user) != 1) {
-    $errorMessage .= "\nDit e-mail adres is niet in gebruik.";
+    $errorMessage .= "\n" . lang('email_not_found');
     $error = true;
   }
 
   if ($error) {
     return getForgotPasswordForm() . '<br />' . nl2br($errorMessage);
   }
+
   $newPassword = createRandomPassword();
-  $emailMessage = new Mail('Bugtracker password reset', '<html><head><title>Password reset</title></head><body>U (of iemand anders) heeft het wachtwoord gereset voor het account met dit e-mailadres.<br />Het nieuwe wachtwoord is nu: '.htmlSafe($newPassword).'<br />Heeft u niet u niet om een nieuw wachtwoord gevraagt, gelieve contact op te nemen met '.getConfigurationValue('webmastermail').'<br />Met vriendelijke groet, Alex de Vries.</body></html>');
+  $emailMessage = new Mail(lang('password_reset_subject'), '<html><head><title>'.lang('password_reset').'</title></head><body>'.lang('password_reset_body1') . htmlSafe($newPassword) . lang('password_reset_body2') . getConfigurationValue('webmastermail') . lang('password_reset_body3')  .'</body></html>');
   $result = $emailMessage->send($mail);
+
   if ($result !== false) {
-    $registerMessage = 'Er is een mail gestuurd met daarin het nieuwe wachtwoord naar &nbsp;'.htmlSafe($mail).'&nbsp;';
+    $registerMessage = lang('password_reset_ok') . htmlSafe($mail).'&nbsp;';
     Database::updateUserPassword($user[0]['id'], $newPassword);
   } else {
-    $registerMessage = getForgotPasswordForm() . '<br />Het wachtwoord is NIET aangepast, omdat de server niet in staat was om een nieuw wachtwoord via e-mail toe te sturen.';
+    $registerMessage = getForgotPasswordForm() . lang('password_reset_error');
   }
   return $registerMessage;
 }
 
-?>
+
