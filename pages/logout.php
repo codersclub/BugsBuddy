459a5bdfea9316bf0c2f082423880a1fa7292635
vot <vvotintsev@gmail.com>
2006-08-24 22:16:03 +0300

    added lang() to pages/logout.php

--- pages/logout.php
+++ pages/logout.php
@@ -5,25 +5,34 @@
 */
 
 function getlogout() {
-  if (isset($_GET['js']) && $_GET['js'] == "no") {
-    require_once(ROOT_DIR.'/pages/home.php');
-    return gethome();
-  } else {
+  if (isset($_GET['js']) && $_GET['js'] == 'yes') {
     require_once(ROOT_DIR.'/pages/home.php');
     require_once(ROOT_DIR.'/pages/login.php');
     // logout executes aditional javascript (is parsed in script.php)
     return Array(gethome(), 'updateLogin("'.addslashes(getLoginHtml()).'");updateLinks("'.str_replace ("\n", "", addslashes(getLinksHtml())).'");');
+  } else {
+//    require_once('pages/home.php');
+//    return gethome();
+
+//DEBUG
+//echo "getlogout started.<br>\n";
+    return logoutInfo();
   }
+}
+
+function logoutInfo() {
+  return '<h1>'.lang('logout').'</h1>'.lang('logout_ok');
 }
 
 /*
  * This function handles the logout before any other output is generated... used by index.php
  */
 function logout() {
-  $_SESSION["loggedIn"] = false; // just to be sure
+  $_SESSION['loggedIn'] = false; // just to be sure
   //unset($_SESSION["username"]);
   unset($_SESSION);
   setcookie('AUTOLOGIN', ''); // expire in 30 days
+
+  return '<h1>'.lang('logout').'</h1>'.lang('logout_ok');
 }
 
-?>
