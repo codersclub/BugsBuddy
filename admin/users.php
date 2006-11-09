6095cd38fcab489a8e90c512c32d2f2fa7e6a126
vot <vvotintsev@gmail.com>
2006-11-09 16:21:43 +0300

    admin/users.php: added lang('users'), and reformatted.

--- admin/users.php
+++ admin/users.php
@@ -14,9 +14,8 @@ function getusers() {
 
 function outputForm($recoverData) {
   $returnValue = '';
-  $returnValue .= "<h1>Users</h1>";
-  
-  
+  $returnValue .= '<h1>'.lang('users').'</h1>';
+
   $result = Database::getAllUsers();
 /*
 echo "<pre>";
@@ -24,10 +23,14 @@ print_r($result);
 echo "</pre>";
 */
   if(!empty($result)) {
-    $returnValue .= '<table style="width: 100%;">'.
-              '<tr>'.
-                '<th>&nbsp;</th><th>&nbsp;</th><th>'. lang('name'). '</th><th>'. lang('email'). '</th><th>' . lang('group') . '</th>'.
-              '</tr>';
+    $returnValue .= '<table style="width: 100%;">
+              <tr>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+                <th>'. lang('name'). '</th>
+                <th>'. lang('email'). '</th>
+                <th>' . lang('group') . '</th>
+              </tr>';
     $i = 1;              
     foreach ($result as $row) {
       if ($i % 2 == 0) {
@@ -35,14 +38,17 @@ echo "</pre>";
       } else {
         $returnValue .= '<tr>';
       }      
-      $returnValue .=   '<td><a href="index.php?'.(isset($_GET['js'])?'js=yes':'').'&page=users&id='.$row['id'].'">' . lang('edit') . '</a></td><td><a href="index.php?'.(isset($_GET['js'])?'js=yes':'').'&page=users&id='.$row['id'].'&delete=true">' . lang('delete') . '</a></td><td>'.htmlSafe($row['name']).'</td><td>'.htmlSafe($row['email']).'</td><td>'.htmlSafe($row['groupname']).'</td>'.
-              '</tr>';
+      $returnValue .=   '<td><a href="index.php?'.(isset($_GET['js'])?'js=yes':'').'&page=users&id='.$row['id'].'">' . lang('edit') . '</a></td>
+                <td><a href="index.php?'.(isset($_GET['js'])?'js=yes':'').'&page=users&id='.$row['id'].'&delete=true">' . lang('delete') . '</a></td>
+                <td>'.htmlSafe($row['name']).'</td><td>'.htmlSafe($row['email']).'</td>
+                <td>'.htmlSafe($row['groupname']).'</td>
+              </tr>';
       $i++;
     }
-    $returnValue .=   '<tr>'.
-                '<td>&nbsp;</td>'.
-              '</tr>'.
-            '</table>';
+    $returnValue .=   '<tr>
+                <td>&nbsp;</td>
+              </tr>
+            </table>';
   }
   
   $id      = 0;
@@ -51,13 +57,13 @@ echo "</pre>";
   $email   = '';
   $description  = '';
   if ($recoverData && isset($_GET) && isset($_GET['groupid']) && isset($_GET['name']) && isset($_GET['email']) && isset($_GET['description'])) {
-    $groupId = $_GET['groupid'];
+    $groupId = intval($_GET['groupid']);
     $name    = $_GET['name'];
     $email   = $_GET['email'];
   }
   
-    if(isset($_GET) && isset($_GET['id'])) {
-    $id = $_GET['id'];
+  if(isset($_GET) && isset($_GET['id'])) {
+    $id = intval($_GET['id']);
     
     $result = Database::getUserById($id);
     
@@ -69,57 +75,71 @@ echo "</pre>";
       }
     }
   }
+
   if (isset($_GET['id']) && !isset($_GET['submitit'])) {
     $thisPage   = "users";
     $currentUrl = getCurrentRequestUrl();
     $currentUrl = explode("?", $currentUrl);
     $currentUrl = $currentUrl[0];
-    $returnValue .= '<table>'.
-              '<tr>'.
-                '<td>'.
-                  '<form action="'.$currentUrl.'" method="get">'.
-                    '<div><input type="hidden" name="submitit" value="true"/></div>'.
-                    '<div><input type="hidden" name="page" value="'.$thisPage.'"/></div>'.
-                    '<div><input type="hidden" name="submitit" value="true"/></div>';
+    $returnValue .= '<table>
+              <tr>
+                <td>
+                  <form action="'.$currentUrl.'" method="get">
+                    <div><input type="hidden" name="submitit" value="true"/></div>
+                    <div><input type="hidden" name="page" value="'.$thisPage.'"/></div>
+                    <div><input type="hidden" name="submitit" value="true"/></div>';
     if (isset($_GET['js'])) {
       $returnValue .= '<div><input type="hidden" name="js" value="yes"/></div>';
     }      
     if(!empty($id)) {
-      $returnValue .=       '<div><input type="hidden" name="id" value="'.$id.'"/></div>';
+      $returnValue .= '<div><input type="hidden" name="id" value="'.$id.'"/></div>';
     }
                   
-    $returnValue .=  '<div class="registerlabel"><label for="name">'. lang('name'). ':</label></div><div class="registerinput"><input type="text" class="" id="name" name="name" size="40" value="'.$name.'"/></div>'.
-                    '<div class="registerlabel"><label for="email">'. lang('email'). ':</label></div><div class="registerinput"><input type="text" class="" id="email" name="email" size="40" value="'.$email.'"/></div>';
-if (getCurrentGroupId() == 3) {
-  $returnValue .= '<div class="registerlabel"><label for="groupid">' . lang('group') . ':</label></div><div class="registerinput"><select class="" id="groupid" name="groupid">'.getGroups($groupId).'</select></div>';
-}
-    $returnValue .=  '<div class="registerlabel"><label for="verzenden">'. lang('send') .':</label></div><div class="registerinput"><input class="" id="verzenden" name="verzenden" type="submit" value="'. lang('send') .'!"/></div>'.
-                  '</form>'.
-                '</td>'.
-              '</tr>'.
-            '</table>';
+    $returnValue .=  '<div class="registerinput">
+                       <label for="name" class="registerlabel">'. lang('name'). ':</label>
+                       <input type="text" class="" id="name" name="name" size="40" value="'.$name.'"/>
+                     </div>
+                     <div class="registerinput">
+                       <label for="email" class="registerlabel">'. lang('email'). ':</label>
+                       <input type="text" class="" id="email" name="email" size="40" value="'.$email.'"/>
+                     </div>';
+    if (getCurrentGroupId() == 3) {
+      $returnValue .= '<div class="registerinput">
+                         <label for="groupid" class="registerlabel">' . lang('group') . ':</label>
+                         <select class="" id="groupid" name="groupid">'.getGroups($groupId).'</select>
+                       </div>';
+    }
+    $returnValue .=  '<div class="registerinput">
+                        <label for="verzenden" class="registerlabel">'. lang('send') .':</label>
+                        <input class="" id="verzenden" name="verzenden" type="submit" value="'. lang('send') .'!"/>
+                      </div>
+                    </form>
+                  </td>
+                </tr>
+              </table>';
   }
-  
   
   return $returnValue;
 }
 
 function handleForm() {
+  // Delete
   if (isset($_GET['delete']) && $_GET['delete']=='true' && isset($_GET['id'])) {
     $id=intval($_GET['id']);
     if (getCurrentGroupId() == 3) {
       Database::deleteUserById(intval($_GET['id']));
     }
-  } else {
-    if (isset($_GET) && isset($_GET['name']) && isset($_GET['email']) && isset($_GET['groupid']) && isset($_GET['id'])) {
-      $name    = $_GET['name'];
-      $email   = $_GET['email'];
-      $groupid = intval($_GET['groupid']);
-      $id      = intval($_GET['id']);
-      Database::updateUser($id, $name, $email, ((getCurrentGroupId()!=3)?null:$groupid));
+
+  // Edit
+  } elseif (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['groupid']) && isset($_GET['id'])) {
+    $name    = $_GET['name'];
+    $email   = $_GET['email'];
+    $groupid = intval($_GET['groupid']);
+    $id      = intval($_GET['id']);
+
+    Database::updateUser($id, $name, $email, ((getCurrentGroupId()!=3)?null:$groupid));
   
-    } else {
-    }
+  } else {
   }
   return outputForm(false);
   
