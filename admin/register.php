23d44ce72e0ecc67ab976a9d7f3400b182109457
vot <vvotintsev@gmail.com>
2006-11-29 18:07:40 +0300

    DEBUG commented

--- admin/register.php
+++ admin/register.php
@@ -4,15 +4,15 @@
   Admin Register Page...
 */
 //DEBUG
-echo '<pre>';
-echo 'admin/register.php:', "\n";
+//echo '<pre>';
+//echo 'admin/register.php:', "\n";
 //echo 'getRegister', "\n";
-echo '_POST='; print_r($_POST);
-echo 'name=', $name, "\n";
-echo 'password=', $password, "\n";
-echo 'email=', $email, "\n";
-echo 'groupid=', $groupid, "\n";
-echo '</pre>';
+//echo '_POST='; print_r($_POST);
+//echo 'name=', $name, "\n";
+//echo 'password=', $password, "\n";
+//echo 'email=', $email, "\n";
+//echo 'groupid=', $groupid, "\n";
+//echo '</pre>';
 //exit;
 
 
